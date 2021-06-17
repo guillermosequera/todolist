5ab52f0ba15e7eb94cc84655522abe64efe96f0b
Guillermo Sequera <sequeracrespo@gmail.com>
2021-06-17 11:03:33 -0400

    itemleft

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+export function Todo({ todo, index, removeTodo }) {
   return (
     <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo d-flex mb-1 p-1 justify-content-between" 
     >
       {todo.text}
 
       <div>
-        <button onClick={() => removeTodo(index)} type="button" className="close border-0 " aria-label="Close">&times;</button>
+        <button onClick={() => removeTodo(index)} type="button" className="close" aria-label="Close">&times;</button>
       </div>
     </div>
+    
   );
 }
 
 function TodoForm({ addTodo}) {
   const [value, setvalue] = useState("");
+  
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -27,11 +28,12 @@ function TodoForm({ addTodo}) {
   setvalue("");
   };
 
+
   return (
     <form onSubmit={handleSubmit}>
       <input 
         type="text"
-        className="input"
+        className="input w-100 border border-light mb-1"
         value={value}
         onChange={e => setvalue(e.target.value)}
       />
@@ -73,10 +75,16 @@ function App() {
     setTodos(newTodos);
   };
 
+  
+
+
+
+
+
   return (
-    <div className="container card">
+    <div className="container" style={{background: "#F8D3D5"}}>
     <h1 className="text-danger"><em>todos</em></h1>
-      <div className="todo-list m-auto">
+      <div className="todo-list m-auto card w-50 p-3" style={{background: "#F8D3D5"}}>
         <TodoForm addTodo={addTodo} />
         {todos.map((todo, index) => (
           <Todo 
@@ -87,6 +95,12 @@ function App() {
             removeTodo={removeTodo}
           />
         ))}
+      <div style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignContent: "stretch",
+                  justifyContent: "flex-end"
+      }}>{todos.length} Left item</div>
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow, configure, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react.16';
+import App, { Todo } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() })
+
+describe("App", () => {
+  describe('Todo', () => {
+    const removeTodo = jest.fn();
+    const index = 5;
+    const todo = {
+      text: 'lala'
+    };
+
+    const wrapper = shallow(
+    <Todo 
+      removeTodo={removeTodo}
+      index={index}
+      todo={todo}
+    />
+    );
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+      expect(removeTodo.mock.calls).toEqual([[5]]);
+  });
 });
